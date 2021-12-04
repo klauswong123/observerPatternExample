1bbae96dc1ae0d0533014eb27c85529d54912a66
klauswong123 <>
2021-12-04 22:55:42 +0800

    [ObserverPatternExample][KlausWong-2] feat: code done for observer

--- src/main/java/com/observerPattern/Main.java
+++ src/main/java/com/observerPattern/Main.java
@@ -8,9 +8,15 @@ public class Main {
         PostOffice hkPostOffice= new PostOffice();
         hkPostOffice.subscriptBy(Jason);
         hkPostOffice.subscriptBy(Klaus);
+        //when
         String mailSubject = "Greeting";
         String mailBody = "Hi Jason, i heard that you are a very good singer.";
         Mail newMailByKlaus = new Mail(Klaus.getName(),Jason.getName(),mailSubject,mailBody);
         hkPostOffice.getNewMail(newMailByKlaus);
+        //when
+        String mailSubject1 = "Greeting";
+        String mailBody1 = "Hi Klaus, of course, I am the best singer ever";
+        Mail newMailByJason = new Mail(Jason.getName(),Klaus.getName(),mailSubject1,mailBody1);
+        hkPostOffice.getNewMail(newMailByJason);
     }
 }
